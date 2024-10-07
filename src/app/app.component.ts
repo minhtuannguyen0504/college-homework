2205921819896509be421a0a254fc2e92028f475
minhtuannguyen0504 <minhtuannguyen0504@gmail.com>
2024-10-07 22:00:20 +0700

    delete extra folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ApiService } from './services/api.service';
+import { UserComponent } from './components/user/user.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [UserComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 
-export class AppComponent {
-  constructor(private apiService: ApiService) {}
-
-  // ngOnInit() {
-  //   this.apiService.getData().subscribe(
-  //     (data: Object) => {
-  //       console.log(data);
-  //     },
-  //     (error: any) => {
-  //       console.error(error);
-  //     }
-  //   );
-  // }
-}
+export class AppComponent {}
 
