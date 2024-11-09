484b33c341ac12dad832a014034b92a334cc77da
Ngoc Viet <134496998+nviet2k2@users.noreply.github.com>
2024-11-09 14:29:39 +0700

    update menu left

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,27 @@ import { CartComponent } from './components/cart/cart.component';
 import { RegisterComponent } from './components/register-form/register.component';
 import { PipeComponent } from './components/pipe/pipe.component';
 import { NewproductComponent } from './NgRxBT/newproduct/newproduct.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ItemComponent } from './components/item/item.component';
 
 export const routes: Routes = [
-    { path: 'weather', component: WeatherComponent },
-    { path: "users", component: UserComponent }, 
-    { path: "products", component: ProductComponent }, 
-    { path: "productsngrx", component: NewproductComponent }, 
-    { path:'product-detail/:id',component:ProductDetailComponent },
-    { path: "cart", component: CartComponent }, 
-    { path: "register", component: RegisterComponent }, 
-    {path: 'pipe', component: PipeComponent}
+    {
+        path: '',
+        redirectTo: 'admin',
+        pathMatch: 'full'
+    },
+    {   path: 'admin', 
+        component: AdminComponent,
+        children: [
+            { path: 'weather', component: WeatherComponent },
+            { path: 'users', component: UserComponent }, 
+            { path: 'products', component: ProductComponent }, 
+            { path: 'productsngrx', component: NewproductComponent }, 
+            { path: 'product-detail/:id', component: ProductDetailComponent },
+            { path: 'cart', component: CartComponent },
+            { path: "register", component: RegisterComponent }, 
+            {path: 'pipe', component: PipeComponent},
+            { path: 'item', component: ItemComponent }
+        ]
+    }
 ];
